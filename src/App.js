4d033709500ddfe263c419f39e95d51d9012392c
Łukasz <nipseyyo@gmail.com>
2021-01-13 21:31:46 +0100

    Basic working version.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import './App.css'
+
+/*
+TODO APP
+1. input todosow
+2. lista todo
+3. usuwanie todo
+*/
+
+function List(props) {
+
+  const deleteItem = key => {
+    const newList = props.itemList.filter(itemObject => {
+      return itemObject.key !== key;
+    });
+    props.updateItemList(newList);
+  };
+
+  return (
+    <div>
+      {props.itemList.map((itemObject) => {
+        return (
+          <div className="itemContainer">
+            <p>{itemObject.item}</p>
+            <button onClick={() => deleteItem(itemObject.key)}>X</button>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 
 function App() {
+  const [currentItem, setCurrentItem] = React.useState();
+  const [itemList, updateItemList] = React.useState([]);
+
+  const onChangeHandler = e => {
+    setCurrentItem(e.target.value);
+  };
+
+  const addItemToList = () => {
+    updateItemList([...itemList, { item: currentItem, key: Date.now() }]);
+    setCurrentItem("");
+  };
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Todo App</p>
       </header>
+      <div >
+        <input placeholder="add something" value={currentItem} onChange={onChangeHandler} />
+        <button onClick={addItemToList}>Add</button>
+      </div>
+      <List itemList={itemList} updateItemList={updateItemList} />
     </div>
   );
 }
