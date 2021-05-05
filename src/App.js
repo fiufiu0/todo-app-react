0c505440f018e947c4ac3512ae61abdb8114c3cb
Łukasz <nipseyyo@gmail.com>
2021-05-05 20:10:05 +0200

    Poprawki. Hooki z wykladow

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
 import * as React from 'react';
 import './App.css'
 
-function ListInside({ deleteItem, toggleCompleted, itemObject, index }) {
+function useLocalStorageState(key, defaultValue) {
+  const [value, setValue] = React.useState(() => {
+    const valueFromLocalStorage = window.localStorage.getItem(key);
+    if (valueFromLocalStorage) {
+      return JSON.parse(valueFromLocalStorage);
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
+  });
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+  return [value, setValue];
+}
 
-  // return (
-  //   <React.Fragment>
-  //     <table>
-  //       <tbody>
-  //         <tr key={itemObject + index} className="itemContainer">
-  //           <td>{itemObject.item}</td>
-  //           <td>
-  //             <input type="checkbox" onChange={() => toggleCompleted(itemObject.index)} checked={itemObject.completed} />
-  //             <button onClick={() => deleteItem(itemObject)}>X</button>
-  //           </td>
-  //         </tr>
-  //       </tbody>
-  //     </table>
-  //   </React.Fragment >
-  // )
+function ListInside({ deleteItem, toggleCompleted, itemObject, index }) {
 
   return (
     <React.Fragment>
       <div key={itemObject + index} className="itemContainer">
         <p>{itemObject.item}</p>
-        <div className="insideItemContainer">
+        <div>
           <input type="checkbox" onChange={() => toggleCompleted(itemObject.index)} checked={itemObject.completed} />
           <button onClick={() => deleteItem(itemObject)}>X</button>
         </div>
       </div>
-    </React.Fragment >
+    </React.Fragment>
   )
 }
-
 
 function List({ itemList, updateItemList, filter }) {
 
@@ -56,26 +53,23 @@ function List({ itemList, updateItemList, filter }) {
     <div>
       {itemList.filter((itemObject) => {
         return (filter === 'all' || (itemObject.completed && filter) === 'completed' || (!itemObject.completed && filter) === 'other');
-      }).map((itemObject, index) => {
+      }).map((itemObject) => {
         return (
-          // tutaj wstawic
-          <ListInside itemObject={itemObject} index={index} deleteItem={deleteItem} toggleCompleted={toggleCompleted} />
+          <ListInside itemObject={itemObject} key={itemObject.index} deleteItem={deleteItem} toggleCompleted={toggleCompleted} />
         )
       })}
     </div>
   )
 }
 
-
-// PANEL WYBORU
 function Filter({ filter, setFilter }) {
   const onChangeHandler = e => {
     setFilter(e.target.value);
   }
   return (
     <React.Fragment>
-      <label for="filter">Filter list </label>
-      <select for="filter" onChange={e => onChangeHandler(e)} value={filter}>
+      <label htmlFor="filter">Filter list </label>
+      <select htmlFor="filter" onChange={e => onChangeHandler(e)} value={filter}>
         <option value="all">ALL</option>
         <option value="completed">COMPLETED</option>
         <option value="other">OTHERS</option>
@@ -85,51 +79,79 @@ function Filter({ filter, setFilter }) {
 };
 
 
-// CAŁY FORMULARZ DODANIA + CZYSZCZENIA
-function Form(props) {
-  const [currentItem, setCurrentItem] = React.useState("");
 
-  const onChangeHandler = (e) => {
-    setCurrentItem(e.target.value);
-  }
+function Form({ setCurrentItem, currentItem, addTodo, clearTodo, isReady }) {
 
-  const onChangeClick = () => {
-    props.addTodo(currentItem);
-    setCurrentItem("");
-  }
+  const inputEl = React.useRef(null);
 
-  const onChangeClear = () => {
-    props.clearTodo([]);
-  }
+  React.useEffect(() => {
+    if (isReady) {
+      inputEl.current.focus();
+    }
+  })
 
   return (
     <React.Fragment>
       <div>
-        <input placeholder="add something" value={currentItem} onChange={onChangeHandler} />
-        <button onClick={onChangeClick}>Add</button>
-        <button onClick={onChangeClear}>Clear list</button>
+        <input ref={inputEl} placeholder="add todo" value={currentItem} onChange={(e) => setCurrentItem(e.target.value)} />
+        <button onClick={addTodo}>Add</button>
+        <button onClick={clearTodo}>Clear list</button>
       </div>
     </React.Fragment>
   )
 }
 
+function useFetch(url) {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url)
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error(error);
+      }
 
-// GŁÓWNA APLIKACJA z komponentów
-function App() {
-  const [itemList, updateItemList] = React.useState([]);
-  const [filter, setFilter] = React.useState('all');
-
-  const getRandom = (length) => {
-    let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (var i = 0; i < length; i++) {
-      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
     }
-    return result;
-  }
+    fetchData();
+  }, [url]);
 
-  const addItemToList = (currentItem) => {
-    updateItemList([...itemList, { item: currentItem, completed: false, index: getRandom(20) }]);
+  return data;
+}
+
+
+function App() {
+  const [itemList, updateItemList] = useLocalStorageState("myKey", []);
+  const [lastId, setLastId] = useLocalStorageState("lastId", 0);
+  const [currentItem, setCurrentItem] = React.useState("");
+  const [filter, setFilter] = React.useState('all');
+  const apiUrl = "https://jsonplaceholder.typicode.com/users/1/todos";
+  const data = useFetch(apiUrl);
+  const memoValue = React.useMemo(() => {
+    const newData = data.map((el) => {
+      const newEl = {
+        index: el.id,
+        completed: el.completed,
+        item: el.title,
+      };
+      return newEl;
+    });
+    return newData;
+  }, [data])
+
+  React.useEffect(() => {
+    if (itemList.length === 0) {
+      setLastId(memoValue.length + 1);
+      updateItemList(memoValue);
+    }
+  }, [memoValue, setLastId, updateItemList, itemList])
+
+  const addItemToList = () => {
+    updateItemList([...itemList, {
+      item: currentItem, completed: false, index: Math.floor(Math.random() * 10000)
+    }]);
+    setCurrentItem("");
   };
 
   const clearTodo = () => {
@@ -140,9 +162,9 @@ function App() {
     <React.Fragment>
       <div className="App">
         <header className="App-header">
-          <h4>TODO LIST</h4>
+          <h4>TODO LIST APP</h4>
         </header>
-        <Form addTodo={addItemToList} clearTodo={clearTodo} />
+        <Form isReady={memoValue.length >= 0} addTodo={addItemToList} clearTodo={clearTodo} currentItem={currentItem} setCurrentItem={setCurrentItem} />
         <Filter filter={filter} setFilter={setFilter} />
         <List itemList={itemList} updateItemList={updateItemList} filter={filter} />
       </div>
