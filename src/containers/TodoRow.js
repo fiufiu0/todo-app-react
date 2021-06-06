8509cb9dabf3d41271356f0d368a777febbbf3d7
Łukasz <nipseyyo@gmail.com>
2021-06-06 20:36:17 +0200

    UseReducer na delete

--- src/containers/TodoRow.js
+++ src/containers/TodoRow.js
@@ -2,8 +2,32 @@ import * as React from 'react';
 import Checkbox from '../components/Checkbox';
 import ButtonX from '../components/ButtonX';
 
-
 function TodoRow({ deleteItem, toggleCompleted, itemObject, index }) {
+
+    const initialState = {
+        check: false,
+    };
+
+    const reducer = (state, action) => {
+
+        console.log(' > state log', state);
+        console.log(' > action log', action);
+
+        switch (action.type) {
+            case 'CHECK':
+                return {
+                    ...state,
+                    check: action.payload,
+                };
+            case 'DELETE':
+                deleteItem(action.payload);
+                return state;
+            default:
+                return state;
+        }
+    }
+
+    const [state, dispatch] = React.useReducer(reducer, initialState);
 
     return (
         <React.Fragment>
@@ -11,11 +35,28 @@ function TodoRow({ deleteItem, toggleCompleted, itemObject, index }) {
                 <p>{itemObject.item}</p>
                 <div>
                     <Checkbox onChange={() => toggleCompleted(itemObject.index)} checked={itemObject.completed} />
-                    <ButtonX onClick={() => deleteItem(itemObject)} />
+                    <ButtonX onClick={() => dispatch({ type: 'DELETE', payload: itemObject })} />
                 </div>
             </div>
         </React.Fragment>
     )
 }
+
+// ZAPASOWE BEZ USEREDUCER
+
+// function TodoRow({ deleteItem, toggleCompleted, itemObject, index }) {
+
+//     return (
+//         <React.Fragment>
+//             <div key={itemObject + index} className="itemContainer">
+//                 <p>{itemObject.item}</p>
+//                 <div>
+//                     <Checkbox onChange={() => toggleCompleted(itemObject.index)} checked={itemObject.completed} />
+//                     <ButtonX onClick={() => deleteItem(itemObject)} />
+//                 </div>
+//             </div>
+//         </React.Fragment>
+//     )
+// }
 
 export default TodoRow;
