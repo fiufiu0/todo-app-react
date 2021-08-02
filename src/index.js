52a530f685a6c0738443537e33c50c862006b886
Łukasz <nipseyyo@gmail.com>
2021-08-02 20:11:40 +0200

    Clean the app from basic react app files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PublicPage from './components/PublicPage';
 import AuthProvider from './components/AuthProvider';
@@ -43,8 +42,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
